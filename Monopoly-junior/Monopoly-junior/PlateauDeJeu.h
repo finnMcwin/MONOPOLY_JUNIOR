240523f598bdde408004878b1f7eeb67cceb9287
Cassoupp <cassandra.sabiude.06@gmail.com>
2025-05-28 16:04:08 +0200

    Plateau

--- Monopoly-junior/Monopoly-junior/PlateauDeJeu.h
+++ Monopoly-junior/Monopoly-junior/PlateauDeJeu.h
@@ -1,6 +1,10 @@
 #pragma once
 #include "Case.h"
 #include <vector>
+#include <string>
+#include "Joueur.h"
+#include "Attraction.h"
+#include "Taxe.h"
 
 class PlateauDeJeu
 {
@@ -11,7 +15,11 @@ public :
 	PlateauDeJeu();
 
 	Case chance(int position);
-
+	Attraction attraction(int prix, bool occupe, Joueur proprietaire, int position, std::string couleur);
+	Taxe taxe(int prix, int position);
+	Taxe depart(int position, int prix); 
+	Case inactive(int position); 
+	Case petitTrain(int position); 
 
 };
 
