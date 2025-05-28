7c923903e82e3696c3c4505e8afe09f2003ff7cf
finnMcwin <120583731+finnMcwin@users.noreply.github.com>
2025-05-28 16:55:59 +0200

    Cartes ajoutées

--- Monopoly-junior/Monopoly-junior/Cartes.h
+++ Monopoly-junior/Monopoly-junior/Cartes.h
@@ -14,6 +14,23 @@ public:
 	Cartes* getNextCarte() const; 
 	void setPreviousCarte(Cartes carte); 
 	void setNextCarte(Cartes carte); 
+	void standGratuitOrange();
+	void standGratuitRouge();
+	void standGratuitRose();
+	void standGratuitBleuClair();
+	void standGratuitJaune();
+	void standGratuitVert();
+	void standGratuitViolet();
+	void vaCaseTobogan();
+	void vaGrandHuit();
+	void vaCafe();
+	void vaCaseDepart();
+	void vaBallet();
+	void vaFeuDartifice();
+	void trainBleu();
+	void trainVert();
+	void trainRouge();
+	void trainJaune();
 
 };
 
