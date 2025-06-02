cd24aef9712e824be724972814633dd1a411e847
Cassoupp <cassandra.sabiude.06@gmail.com>
2025-06-02 09:59:29 +0200

    suite plateau

--- Monopoly-junior/Monopoly-junior/PlateauDeJeu.h
+++ Monopoly-junior/Monopoly-junior/PlateauDeJeu.h
@@ -13,14 +13,19 @@ private:
 	std::vector <Case*> listeCases;
 	
 	void caseAttractions(int position, Joueur* joueurActif); 
-	void caseTaxe(int position, Joueur* joueurActif);
+	void caseTaxe(int position, Joueur* joueurActif, int* argentCentre);
+	void caseAllerCafe(Joueur* joueurActif, int position, int* argentCentre);
+	void caseCafe(int position, Joueur* joueurActif); 
+	void caseFortune(int position, Joueur* joueurActif, int* argentCentre);
+	void casePetitTrain(int position, Joueur* joueurActif, int* argentCentre);
+	int rollDiceP(); 
 
 public : 
 	PlateauDeJeu();
 
 	void setCase(Case* newCase);
 	
-	//void effetCase(Joueur* joueurActif, int position); 
+	void effetCase(int position, Joueur* joueurActif, int* argentCentre); 
 	Case* getCase(int position);
 
 };
